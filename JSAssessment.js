afabed41484dc978b2f12e42677827d49f7f9a2f
QuincyJefferson <astrocundy@gmail.com>
2021-10-19 13:39:34 -0400

    added custom error message

--- JSAssessment.js
+++ JSAssessment.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
-
+    // Checks if favorite song input is filled with content
     $("input[name='question1']").on("input", function() {
+        var re = /^ *$/;
         let input=$(this);
+        var isBlank = re.test(input.val());
         let is_name=input.val();
-        if(is_name){input.removeClass("invalid").addClass("valid");}
+        if(is_name && !isBlank){input.removeClass("invalid").addClass("valid");}
         else {input.removeClass("valid").addClass("invalid");}
     });
-    //
+    // Checks if favorite color input is filled with content
     $("input[name='question2']:first").on("input", function() {
         let input=$(this);
         let is_name=input.val();
         if(is_name){input.removeClass("invalid").addClass("valid");}
         else {input.removeClass("valid").addClass("invalid");}
     });
-    // Verify the first two questions have any content (need to check for blank spaces)
+    // Verify the first two questions have content
     $("input[type='submit']").click(function(event) {
-        if($("input[name='question1']").val() && $("input[name='question2']:first").val()) {
+        var re = /^ *$/;
+        var isSongBlank = re.test($("input[name='question1']").val());
+        var isColorBlank = re.test($("input[name='question2']:first").val());
+        if($("input[name='question1']").val() && $("input[name='question2']:first").val() && !isSongBlank && !isColorBlank) {
             return;
         } else { 
             event.preventDefault();
-            alert('Please make sure questions 1 and 2 are filled out.');
+            $("#content").append("<div class='error'><h3>Error</h3><p>Please fill out questions 1 and 2 before trying to submit.</p><button class='okayBtn'>Okay</button></div>")
+            $(".okayBtn").click(function() {
+                $(".error").hide();
+            })
         }
     });
-    // Using replaceWith to change favorite movie input with dropdown menu instead
+    // Using replaceWith to change favorite movie input with select menu instead
     $("input[name='question2']").eq(1).replaceWith("<select name='question2' id='movies'> <option value='starWars'>Star Wars </option> <option value='lordOfTheRings'>Lord of the Rings</option> <option value='harryPotter'>Harry Potter</option> <option value='hungerGames'>Hunger Games </option>");
 
 });
